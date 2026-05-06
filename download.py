4d145f43bd4d258c11b757fec3b1ccee20b5ea28
artajmir3 <artajmir3@yahoo.com>
2026-05-06 09:45:01 -0700

    Delete download.py

--- download.py
+++
@@ -1,8 +0,0 @@
-import urllib.request
-
-file = open("names.txt", "r")
-lines = file.readlines()
-for line in lines:
-	url = "https://files.rcsb.org/download/%s.cif"%(line.strip(),)
-	urllib.request.urlretrieve(url, "%s.cif"%(line.strip(),))
-	print(url)
